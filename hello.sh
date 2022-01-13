465d13fe9b24cdb6a0cab4c89bfdab9a131b290d
Pamoda Sandeepa <pamoda.sandeepa@ifsworld.com>
2022-01-13 11:59:20 +0530

    first commit

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#! usr/bin/bash
+
+declare -A map
+
+map["prac1"]="Practice1"
+
+git init
+git add hello.txt
+git commit -m "committed"
+
+if [  ]; then
+    echo "Unable to access '........' "
+    exit 1
+fi
+
+do
+          git push -u origin master
+done
